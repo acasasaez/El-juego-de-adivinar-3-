527eced24433d52e07989279019a4fda42986ce2
acasasaez <acasasae@myuax.com>
2021-11-05 17:26:27 +0100

    primera y segunda condición

--- eljuego.py
+++ eljuego.py
@@ -3,4 +3,9 @@ numero = random.randint(0,99)
 intento = (int(input("Elija un número del 1 al 99:")))
 numero_intento = 0
 while intento != numero:
-    
+    if intento < numero:
+        print ("Demasiado pequeño.")
+        numero_intento = numero_intento + 1
+    if intento > numero:
+        print( "Demasiado grande.")
+        numero_intento = numero_intento +1
