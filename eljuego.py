6bd80fef30eca3d56f30ccda9833d91ee78ac6a3
acasasaez <acasasae@myuax.com>
2021-11-05 17:38:41 +0100

    arreglo de cambios

--- eljuego.py
+++ eljuego.py
@@ -1,14 +1,16 @@
 import random
 numero = random.randint(0,99)
-intento = (int(input("Elija un numero del 1 al 99:")))
+intento = int(input("Elija un numero del 1 al 99:"))
 numero_intento = 0
 while intento != numero:
     if intento < numero:
         print ("Demasiado pequeño.")
         numero_intento = numero_intento + 1
+        intento = int (input("Elija otro numero:"))
     if intento > numero:
         print( "Demasiado grande.")
         numero_intento = numero_intento +1
+        intento = int (input("Elija otro numero:"))
     if intento == numero:
         print("Enhorabuena, has acertado.")
         numero_intento = numero_intento +1
