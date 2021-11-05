f14c35228d590b2fae2677c7d5e11e96ce76bcb0
acasasaez <acasasae@myuax.com>
2021-11-05 17:31:57 +0100

    Update eljuego.py

--- eljuego.py
+++ eljuego.py
@@ -1,6 +1,6 @@
 import random
 numero = random.randint(0,99)
-intento = (int(input("Elija un número del 1 al 99:")))
+intento = (int(input("Elija un numero del 1 al 99:")))
 numero_intento = 0
 while intento != numero:
     if intento < numero:
