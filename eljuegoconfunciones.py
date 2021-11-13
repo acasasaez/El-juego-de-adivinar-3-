b159db700d0c8fee88308c87737d7b5ecebc4e08
acasasaez <acasasae@myuax.com>
2021-11-13 20:32:50 +0100

    def2

--- eljuegoconfunciones.py
+++ eljuegoconfunciones.py
@@ -5,3 +5,9 @@ def ns (c):
     while c != ("s") and c!= ("n"):
         print(chr(7)); c= input ("Escriba solo `\n\`o `\s\` según su opción:")
     return (c)
+def OKI (n):
+    try:
+       n= int(n) 
+    except:
+         n= OKI (input ("Caracter no valido:"))
+    return n
