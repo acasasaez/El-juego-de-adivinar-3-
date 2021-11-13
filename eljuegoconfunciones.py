28431ed857ad6698387c185a9421a4a6e9bca552
acasasaez <acasasae@myuax.com>
2021-11-13 22:31:50 +0100

    act.

--- eljuegoconfunciones.py
+++ eljuegoconfunciones.py
@@ -25,7 +25,7 @@ def paramar(n):
     m=n-1
     return m
 while True:
-    marca= pickle.load(open("mejor_marca", "rb"))
+    marca= pickle.load(open ("mejor_marca","rb"))
     print ("ADIVINA EL NUMERO")
     print ("""En este juego el usuario debe adivinar un numero, escogido al azar por la computadora, entre un rango determinado""")
     print("")
@@ -70,9 +70,18 @@ Nivel 3: Entre 0 y 1000000""")
         print ("Felicidades, lo has logrado")   
         print ("Número de intentos:",intentos,sing_plu(intentos))
         posi_marca= paramar (level)
-    if intentos < marca [posi_marca]:
-        marca [posi_marca] = intentos
-        pickle.dump(marca,open("mejor_marca","wb"))
-        print ("¡NUEVO RECORD!")
+        if intentos < marca [posi_marca]:
+            marca [posi_marca] = intentos
+            pickle.dump(marca,open("mejor_marca","wb"))
+            print ("¡NUEVO RECORD!")
     print ("Mejor marca para este nivel:", marca[posi_marca])    
-    
+    if marca[posi_marca] == 1:
+        marca[posi_marca] = MAX
+        pickle.dump(marca,open("mejor_marca", "wb"))
+    conti = ns(input("¿Jugar otra vez?:"))
+    if conti == ("n"):
+        break
+    try:
+        subprocess.call (["cmd.exe","/C","cls"])
+    except:
+        continue
