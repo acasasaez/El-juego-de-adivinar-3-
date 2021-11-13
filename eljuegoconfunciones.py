5065298c5111c815087950f4a0a812bfb7c70aeb
acasasaez <acasasae@myuax.com>
2021-11-13 20:40:49 +0100

    def3

--- eljuegoconfunciones.py
+++ eljuegoconfunciones.py
@@ -11,3 +11,8 @@ def OKI (n):
     except:
          n= OKI (input ("Caracter no valido:"))
     return n
+def limites (n,MAX):
+     while n<0 or n>MAX:
+         n = OKI (input("ERROR:El numero ha de estar entre 0 y "+str(""+str(MAX)+"): ")))
+     return n  
+       
